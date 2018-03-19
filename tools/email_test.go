f9c02749ba8d77c5abec01840e2f9009101fe580
andy <andy.zhangtao@hotmail.com>
2018-03-19 08:48:29 +0800

    修改Email测试案例
    Commit by Andy.Zhang
    Email: ztao8607@gmail.com

--- tools/email_test.go
+++ tools/email_test.go
@@ -13,8 +13,8 @@ func TestEmail_SendEmail(t *testing.T) {
 		Password: os.Getenv("SS_PASS_WORD"),
 		Port:     587,
 		Dest:     []string{os.Getenv("SS_DEST_EMAIL")},
-		Content:  "A letter for test",
-		Header:   "From Golang Test Units",
+		Content:  "<h1>A letter for test</h1><br/><span>This is email content</span>",
+		Header:   "From Golang Test Units - Email Subject",
 	}
 
 	err := e.SendEmail()
