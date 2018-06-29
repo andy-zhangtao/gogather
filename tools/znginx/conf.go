768ea9899d1fd830104eeb2a78bfc2b6d70552c7
andy <ztao8607@gmail.com>
2018-06-29 16:12:51 +0800

    提供nginx文件相关辅助类函数
    Signed-off-by: andy <ztao8607@gmail.com>

---
+++ tools/znginx/conf.go
@@ -0,0 +1,160 @@
+package znginx
+
+import (
+	"regexp"
+	"strings"
+	"bufio"
+	"errors"
+	"fmt"
+)
+
+//ExtractHosts 从nginx server片段中抽取domain
+//nginx server片段
+func ExtractHosts(nginx string) (hosts []string) {
+	re := regexp.MustCompile("server_name(.*);")
+	h := re.FindStringSubmatch(nginx)
+	if len(h) == 1 {
+		return
+	}
+
+	_hosts := strings.Split(strings.TrimSpace(h[1]), " ")
+	for _, h := range _hosts {
+		if strings.TrimSpace(h) != "" {
+			hosts = append(hosts, h)
+		}
+	}
+	return
+}
+
+//ExtractUpstream 从nginx server片段中抽取upstream片段
+//nginx server片段
+func ExtractUpstream(nginx string) (upstream []string) {
+	scanner := bufio.NewScanner(strings.NewReader(nginx))
+
+	hasUps := false
+	var ups string = ""
+
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), "upstream") {
+			hasUps = true
+			ups = scanner.Text() + "\n"
+			continue
+		}
+
+		if hasUps {
+			ups += scanner.Text() + "\n"
+			if strings.Contains(scanner.Text(), "}") {
+				hasUps = false
+				upstream = append(upstream, ups)
+			}
+		}
+	}
+
+	return
+}
+
+//ExtractLocation 从nginx server片段中抽取domain和location的映射数据
+func ExtractLocation(nginx string) (location map[string][]string) {
+	hosts := ExtractHosts(nginx)
+
+	location = make(map[string][]string)
+
+	loc := extractLocation(nginx)
+
+	for _, h := range hosts {
+		location[h] = loc
+	}
+
+	return
+}
+
+func extractLocation(nginx string) (location []string) {
+	scanner := bufio.NewScanner(strings.NewReader(nginx))
+
+	hasLoc := false
+	var loc string = ""
+
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), "location") {
+			hasLoc = true
+			loc = scanner.Text() + "\n"
+			continue
+		}
+
+		if hasLoc {
+			loc += scanner.Text() + "\n"
+			if strings.Contains(scanner.Text(), "}") {
+				hasLoc = false
+				location = append(location, loc)
+			}
+		}
+	}
+
+	return
+}
+
+//MergeServerF1 标准Server合并模式
+//每个server片段中server_name 只能是单域名，在多域名情况下使用此函数会导致合并后的业务语义错误
+//通过isMerge返回是否发生了合并行为
+func MergeServerF1(nginx1, nginx2 string, nginxs ...string) (nginx string, isMerge bool, err error) {
+	isMerge = false
+
+	host1 := ExtractHosts(nginx1)
+	ups2 := ExtractUpstream(nginx2)
+	host2 := ExtractHosts(nginx2)
+
+	if len(host1) > 1 || len(host2) > 1 {
+		err = errors.New(fmt.Sprintf("MergeServerF1 Only Merge Single Domain Server"))
+		return
+	}
+
+	if host1[0] == host2[0] {
+		isMerge = true
+		locMap2 := ExtractLocation(nginx2)
+		// 需要合并
+
+		// 先合并location
+		nginx1 = strings.TrimSpace(nginx1)
+
+		_nginx := nginx1[:strings.LastIndex(nginx1, "}")]
+		_nginx += "\n"
+		for _, l := range locMap2[host2[0]] {
+			_nginx += l
+		}
+		_nginx += "}\n"
+
+		// 再合并upstream
+		nginx = strings.Join(ups2, "\n") + _nginx
+
+	}
+
+	for _, n := range nginxs {
+		host := ExtractHosts(n)
+		if len(host) > 1 {
+			err = errors.New(fmt.Sprintf("MergeServerF1 Only Merge Single Domain Server"))
+			return
+		}
+
+		if host1[0] == host[0] {
+			isMerge = true
+			locMap := ExtractLocation(n)
+			ups := ExtractUpstream(n)
+
+			_nginx := nginx[:strings.LastIndex(nginx, "}")]
+
+			_nginx += "\n"
+
+			for _, l := range locMap[host[0]] {
+				_nginx += l
+			}
+
+			_nginx += "}\n"
+
+			nginx = strings.Join(ups, "\n") + _nginx
+		}
+
+	}
+
+	nginx += "\n#Auto Merge By GoGather \n"
+	return
+}
---
+++ tools/znginx/conf_test.go
@@ -0,0 +1,247 @@
+package znginx
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+	)
+
+func TestExtractHosts(t *testing.T) {
+	var nginx = `
+upstream tomcats9540 {
+    server 192.168.1.216:8000;
+}
+
+#admin
+server {
+	server_name  www.chinazt.cc  blog.chinazt.cc;
+
+	location / {
+		index index.html index.htm index.jsp;
+		proxy_pass         http://tomcats9540;
+		proxy_redirect     off;
+		proxy_set_header   Host             $host;
+		proxy_set_header   X-Real-IP        $remote_addr;
+		proxy_set_header  X-Forwarded-For  $proxy_add_x_forwarded_for;
+		client_max_body_size       10m;
+		client_body_buffer_size    128k;
+		proxy_connect_timeout      90;
+		proxy_send_timeout         90;
+		proxy_read_timeout         90;
+		proxy_buffer_size          4k;
+		proxy_buffers              4 32k;
+		proxy_busy_buffers_size    64k;
+		proxy_temp_file_write_size 64k;
+	}
+}
+	
+`
+
+	hosts := ExtractHosts(nginx)
+
+	assert.Equal(t, 2, len(hosts))
+	assert.EqualValues(t, "www.chinazt.cc", hosts[0])
+	assert.EqualValues(t, "blog.chinazt.cc", hosts[1])
+}
+
+func TestExtractUpstream(t *testing.T) {
+	var nginx = `
+upstream tomcats9540 {
+    server 192.168.1.216:8000;
+}
+
+
+#admin
+server {
+	server_name  www.chinazt.cc  blog.chinazt.cc;
+
+	location / {
+		index index.html index.htm index.jsp;
+		proxy_pass         http://tomcats9540;
+		proxy_redirect     off;
+		proxy_set_header   Host             $host;
+		proxy_set_header   X-Real-IP        $remote_addr;
+		proxy_set_header  X-Forwarded-For  $proxy_add_x_forwarded_for;
+		client_max_body_size       10m;
+		client_body_buffer_size    128k;
+		proxy_connect_timeout      90;
+		proxy_send_timeout         90;
+		proxy_read_timeout         90;
+		proxy_buffer_size          4k;
+		proxy_buffers              4 32k;
+		proxy_busy_buffers_size    64k;
+		proxy_temp_file_write_size 64k;
+	}
+}
+
+upstream tomcats9541 {
+    server 192.168.1.216:8000;
+}
+
+`
+
+	ups := ExtractUpstream(nginx)
+
+	assert.Equal(t, 2, len(ups))
+}
+
+func TestExtractLocation(t *testing.T) {
+	var nginx = `
+upstream tomcats9540 {
+    server 192.168.1.216:8000;
+}
+
+
+#admin
+server {
+	server_name  www.chinazt.cc  blog.chinazt.cc;
+
+	location / {
+		index index.html index.htm index.jsp;
+		proxy_pass         http://tomcats9540;
+		proxy_redirect     off;
+		proxy_set_header   Host             $host;
+		proxy_set_header   X-Real-IP        $remote_addr;
+		proxy_set_header  X-Forwarded-For  $proxy_add_x_forwarded_for;
+		client_max_body_size       10m;
+		client_body_buffer_size    128k;
+		proxy_connect_timeout      90;
+		proxy_send_timeout         90;
+		proxy_read_timeout         90;
+		proxy_buffer_size          4k;
+		proxy_buffers              4 32k;
+		proxy_busy_buffers_size    64k;
+		proxy_temp_file_write_size 64k;
+	}
+
+	location /login {
+		index index.html index.htm index.jsp;
+		proxy_pass         http://tomcats9540;
+		proxy_redirect     off;
+		proxy_set_header   Host             $host;
+		proxy_set_header   X-Real-IP        $remote_addr;
+		proxy_set_header  X-Forwarded-For  $proxy_add_x_forwarded_for;
+		client_max_body_size       10m;
+		client_body_buffer_size    128k;
+		proxy_connect_timeout      90;
+		proxy_send_timeout         90;
+		proxy_read_timeout         90;
+		proxy_buffer_size          4k;
+		proxy_buffers              4 32k;
+		proxy_busy_buffers_size    64k;
+		proxy_temp_file_write_size 64k;
+	}
+}
+
+`
+
+	location := ExtractLocation(nginx)
+
+	assert.EqualValues(t, 2, len(location))
+	assert.EqualValues(t, 2, len(location["www.chinazt.cc"]))
+	assert.EqualValues(t, 2, len(location["blog.chinazt.cc"]))
+}
+
+func TestMergeServerF1(t *testing.T) {
+	var nginx1 = `
+upstream tomcats9540 {
+    server 192.168.1.216:8000;
+}
+
+
+#admin
+server {
+	server_name  www.chinazt.cc;
+
+	location / {
+		index index.html index.htm index.jsp;
+		proxy_pass         http://tomcats9540;
+		proxy_redirect     off;
+		proxy_set_header   Host             $host;
+		proxy_set_header   X-Real-IP        $remote_addr;
+		proxy_set_header  X-Forwarded-For  $proxy_add_x_forwarded_for;
+		client_max_body_size       10m;
+		client_body_buffer_size    128k;
+		proxy_connect_timeout      90;
+		proxy_send_timeout         90;
+		proxy_read_timeout         90;
+		proxy_buffer_size          4k;
+		proxy_buffers              4 32k;
+		proxy_busy_buffers_size    64k;
+		proxy_temp_file_write_size 64k;
+	}
+}
+`
+	var nginx2 = `
+upstream tomcats9541 {
+    server 192.168.1.213:8000;
+}
+
+
+#admin
+server {
+	server_name  www.chinazt.cc;
+
+	location ~* /login {
+		index index.html index.htm index.jsp;
+		proxy_pass         http://tomcats9541;
+		proxy_redirect     off;
+		proxy_set_header   Host             $host;
+		proxy_set_header   X-Real-IP        $remote_addr;
+		proxy_set_header  X-Forwarded-For  $proxy_add_x_forwarded_for;
+		client_max_body_size       10m;
+		client_body_buffer_size    128k;
+		proxy_connect_timeout      90;
+		proxy_send_timeout         90;
+		proxy_read_timeout         90;
+		proxy_buffer_size          4k;
+		proxy_buffers              4 32k;
+		proxy_busy_buffers_size    64k;
+		proxy_temp_file_write_size 64k;
+	}
+}
+`
+	var nginx3 = `
+	upstream tomcats9542 {
+	   server 192.168.1.211:8000;
+	}
+	
+	
+	#admin
+	server {
+		server_name  www.chinazt.cc;
+	
+		location ~* /logout {
+			index index.html index.htm index.jsp;
+			proxy_pass         http://tomcats9542;
+			proxy_redirect     off;
+			proxy_set_header   Host             $host;
+			proxy_set_header   X-Real-IP        $remote_addr;
+			proxy_set_header  X-Forwarded-For  $proxy_add_x_forwarded_for;
+			client_max_body_size       10m;
+			client_body_buffer_size    128k;
+			proxy_connect_timeout      90;
+			proxy_send_timeout         90;
+			proxy_read_timeout         90;
+			proxy_buffer_size          4k;
+			proxy_buffers              4 32k;
+			proxy_busy_buffers_size    64k;
+			proxy_temp_file_write_size 64k;
+		}
+	}
+	`
+
+	nginx, isMerge, err := MergeServerF1(nginx1, nginx2)
+	assert.Nil(t, err)
+	locMap := ExtractLocation(nginx)
+	assert.EqualValues(t, 1, len(locMap))
+	assert.EqualValues(t, true, isMerge)
+	assert.EqualValues(t, 2, len(locMap["www.chinazt.cc"]))
+
+	nginx, _, err = MergeServerF1(nginx1, nginx2, nginx3)
+	assert.Nil(t, err)
+	locMap = ExtractLocation(nginx)
+	assert.EqualValues(t, 1, len(locMap))
+	assert.EqualValues(t, true, isMerge)
+	assert.EqualValues(t, 3, len(locMap["www.chinazt.cc"]))
+
+}
